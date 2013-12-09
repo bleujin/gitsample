aa749a49a0dfaef79f3985b880576b079cd76f91
bleujin <bleujin@gmail.com>
2013-12-09 11:16:24 +0900

    first

---
+++ gitsample/test/net/ion/bleujin/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package net.ion.bleujin;
+
+import javax.swing.DebugGraphics;
+
+import net.ion.framework.util.Debug;
+
+import junit.framework.TestCase;
+
+public class TestHelloWorld extends TestCase{
+
+	
+	public void testHello() throws Exception {
+		System.out.println("Hello") ;
+	}
+}
