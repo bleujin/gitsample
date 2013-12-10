f452eaf15a3d0d996dde9ad67cba256396835680
bleujin <bleujin@bleujin-PC>
2013-12-10 11:25:17 +0900

    ed ed

--- gitsample/test/net/ion/bleujin/TestHelloWorld.java
+++ gitsample/test/net/ion/bleujin/TestHelloWorld.java
@@ -7,6 +7,6 @@ public class TestHelloWorld extends TestCase{
 	
 	public void testHello() throws Exception {
 		System.out.println("Hello") ;
-		System.out.print(" Ed Ed Ed ED") ;
+		System.out.print(" Ed Ed Ed ED ED") ;
 	}
 }
